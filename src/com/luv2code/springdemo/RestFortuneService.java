ffa0fae9bfda1311181d05949e7b009b6b054a32
Ankit <ankitthakur1998@gmail.com>
2022-04-13 17:20:38 +0530

    @Qualifier (randomFortuneService) is working fine

--- src/com/luv2code/springdemo/RestFortuneService.java
+++ src/com/luv2code/springdemo/RestFortuneService.java
@@ -3,7 +3,7 @@ package com.luv2code.springdemo;
 import org.springframework.stereotype.Component;
 
 @Component
-public class RestFortuneService implements FortuneService {
+public class RESTFortuneService implements FortuneService {
 
 	@Override
 	public String getFortune() {
