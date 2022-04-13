f1909f63b0be10db6689f6400cf4a4c5f1bcaf54
Ankit <ankitthakur1998@gmail.com>
2022-04-13 15:00:27 +0530

    @Autowired applied to the setter method for Setter injection

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -8,10 +8,23 @@ public class TennisCoach implements Coach {
 	
 	private FortuneService fortuneService;   //Defining pvt field
 
-	@Autowired
-	public TennisCoach(FortuneService theFortuneService) {
-		fortuneService = theFortuneService;  //Constructor created here but will executed by driver class
+	//define a default constructor
+	public TennisCoach() {
+		System.out.println(">> TennisCoach : inside default constructor");
 	}
+	
+	//define a setter method
+	@Autowired //spring will scan for a component(class) that implements FortuneService Interface
+	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println(">> TennisCoach : inside setFortuneService() method");
+		fortuneService = theFortuneService;
+	}
+	
+	
+//	@Autowired  //Spring will scan for a component(class) that implements FortuneService interface // HappyFortuneService meets the requiremen
+//	public TennisCoach(FortuneService theFortuneService) {
+//		fortuneService = theFortuneService;  //Constructor created here but will executed by driver class
+//	}
 	
 	@Override
 	public String getDailyWorkout() {
