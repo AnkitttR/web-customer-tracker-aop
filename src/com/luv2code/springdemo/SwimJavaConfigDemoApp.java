2a6a200f24fe1eb832118cf41e490de21771a516
Ankit <ankitthakur1998@gmail.com>
2022-04-14 22:49:38 +0530

    Two new call methods added in SwimJavaConfigDemoApp

--- src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
+++ src/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
@@ -11,13 +11,19 @@ public class SwimJavaConfigDemoApp {
 				new AnnotationConfigApplicationContext(SportConfig.class);
 		
 		//get the bean from spring container
-		Coach theCoach = context.getBean("swimCoach",Coach.class);
+		SwimCoach theCoach = context.getBean("swimCoach",SwimCoach.class);
 		
 		//call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		
 		//call method to get the daily fortune
 		System.out.println(theCoach.getDailyFortune());
+		
+		//call our new SwimCoach methods ... has the values injected from the sport.properties
+		
+		System.out.println("email: " +theCoach.getEmail());
+		
+		System.out.println("team :"+theCoach.getTeam());
 		
 		//close the context
 		context.close();
