2f4e6f0fe8c53e7c5f58af7f320fe39998d30a23
Ankit <ankitthakur1998@gmail.com>
2022-04-14 22:39:41 +0530

    @Value is used for injecting values from sport.properties file

--- src/com/luv2code/springdemo/SwimCoach.java
+++ src/com/luv2code/springdemo/SwimCoach.java
@@ -1,8 +1,16 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 
 	private FortuneService fortuneService; //Defining private field fortuneService
+	
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
 	
 	public SwimCoach(FortuneService theFortuneService) {
 		fortuneService = theFortuneService;  //Constructor is defined only not executed here	
