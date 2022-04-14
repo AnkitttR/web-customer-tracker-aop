b869f1e3789cba7a9b0d6d522de0c348a39b09d5
Ankit <ankitthakur1998@gmail.com>
2022-04-14 10:56:28 +0530

    New class SwimJavaConfigDemoApp created

--- src/com/luv2code/springdemo/SportConfig.java
+++ src/com/luv2code/springdemo/SportConfig.java
@@ -1,5 +1,6 @@
 package com.luv2code.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,4 +8,21 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("com.luv2code.springdemo")
 public class SportConfig {
 
+	// define bean for our sad fortune service
+	//sadFortuneService() method name will be beanId
+	@Bean
+	public FortuneService sadFortuneService() {
+		return new SadFortuneService();
+		
+	}
+	
+	// define bean for our swim coach & inject dependency
+	@Bean
+	public Coach swimCoach() {    //Bean for our SwimCoach class created with id "swimCoach"
+								//First letter is small
+		return new SwimCoach(sadFortuneService()); //passing bean which is created in line 13 above
+		
+	}
+	
+	
 }
--- src/com/luv2code/springdemo/SwimCoach.java
+++ src/com/luv2code/springdemo/SwimCoach.java
@@ -2,16 +2,24 @@ package com.luv2code.springdemo;
 
 public class SwimCoach implements Coach {
 
+	private FortuneService fortuneService; //Defining private field fortuneService
+	
+	public SwimCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;  //Constructor is defined only not executed here	
+	}
+	
+	
+	
 	@Override
 	public String getDailyWorkout() {
 		
-		return null;
+		return "Swim 1000 meters as a warm up.";
 	}
 
 	@Override
 	public String getDailyFortune() {
 		
-		return null;
+		return fortuneService.getFortune();
 	}
 
 }
