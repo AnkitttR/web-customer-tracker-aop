fae1fbe8d380606c2f17c6287287ab8e4bd4d667
Ankit <ankitthakur1998@gmail.com>
2022-04-27 21:33:03 +0530

    / do it again!

--- src/com/luv2code/aopdemo/MainDemoApp.java
+++ src/com/luv2code/aopdemo/MainDemoApp.java
@@ -18,6 +18,12 @@ public class MainDemoApp {
 		theAccountDAO.addAccount(); //AccountDAO class ke under addAccount method hai, lekin usko
 		          //access karne ke liye AccountDAO class ke variable theAccountDAO ka use karenge
 		
+		// do it again!
+		System.out.println("\n let's call it again!\n");
+		
+		//call the business method again
+		theAccountDAO.addAccount();
+		
 		//close the context
 		context.close();
 	}
