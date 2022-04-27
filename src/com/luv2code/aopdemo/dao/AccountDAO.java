2713237bbedd87ee061d52c8560bde936e4566d0
Ankit <ankitthakur1998@gmail.com>
2022-04-27 20:40:38 +0530

    @ComponentScan for scanning a package

--- src/com/luv2code/aopdemo/dao/AccountDAO.java
+++ src/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -2,7 +2,7 @@ package com.luv2code.aopdemo.dao;
 
 import org.springframework.stereotype.Component;
 
-@Component    //spring will find it automatically using component scanning
+@Component    //spring will find it automatically using component scanning //Hey this is a component
 public class AccountDAO {
 
 	public void addAccount() {
