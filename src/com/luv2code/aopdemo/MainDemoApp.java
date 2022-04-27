4c650da156f173fbb33dfd5630a7d2c66d6e29eb
Ankit <ankitthakur1998@gmail.com>
2022-04-27 21:03:30 +0530

    /Hey context give me the bean from AccountDAO class

--- src/com/luv2code/aopdemo/MainDemoApp.java
+++ src/com/luv2code/aopdemo/MainDemoApp.java
@@ -2,16 +2,20 @@ package com.luv2code.aopdemo;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.luv2code.aopdemo.dao.AccountDAO;
+
 public class MainDemoApp {
 
 	public static void main(String[] args) {
 		
 		//read spring config java class
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
 		
 		//get the bean from spring container
-		
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+									//Hey context give me the bean from AccountDAO class
 		//call the business method
+		theAccountDAO.addAccount(); 
 		
 		//close the context
 
