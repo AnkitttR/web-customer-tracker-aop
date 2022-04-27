2590250c31d6e96571261f4677c4d0767e68720a
Ankit <ankitthakur1998@gmail.com>
2022-04-27 21:26:56 +0530

    first AOP is ready for running

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,6 +1,7 @@
 package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -10,4 +11,10 @@ public class MyDemoLoggingAspect {
 	// this is where we add all of our related advices for logging
 	
 	//let's start with an @Before advice
+	
+	@Before("execution(public void addAccount())") //any method call void addAccount() please call this
+	public void beforeAddAccountAdvice() {
+		
+		System.out.println("\n=====>>> Executing @Before advice on addAccount()");
+	}
 }
