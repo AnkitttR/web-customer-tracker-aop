c5315dcafe29bcf806c94b5a907ce297459cdb5e
Ankit <ankitthakur1998@gmail.com>
2022-04-27 20:26:22 +0530

    @Component    //spring will find it automatically using component scanning

--- src/com/luv2code/aopdemo/dao/AccountDAO.java
+++ src/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -1,5 +1,8 @@
 package com.luv2code.aopdemo.dao;
 
+import org.springframework.stereotype.Component;
+
+@Component    //spring will find it automatically using component scanning
 public class AccountDAO {
 
 }
