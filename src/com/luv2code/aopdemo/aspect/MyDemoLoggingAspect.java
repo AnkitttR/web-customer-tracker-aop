7b1cb2745bcaec4deb478f1edd6595f1b2232021
Ankit <ankitthakur1998@gmail.com>
2022-04-28 22:24:27 +0530

    /loop thru args

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,11 +1,15 @@
 package com.luv2code.aopdemo.aspect;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import com.luv2code.aopdemo.dao.Account;
 
 @Aspect
 @Component
@@ -30,12 +34,23 @@ public class MyDemoLoggingAspect {
 		System.out.println("Method: " + methodSig);
 		
 		//display the method arguments
-		
-		
+				
 		//get args
+		Object[] args = theJoinPoint.getArgs();
 		
 		//loop thru args
-		
+		for (Object tempArg: args) {
+			System.out.println(tempArg);
+			
+			if (tempArg instanceof Account) {
+				//downcast and print Account specific stuff 
+				
+				Account theAccount = (Account) tempArg;
+				
+				System.out.println("account name: " + theAccount.getName());
+				System.out.println("account level: " + theAccount.getLevel());
+			}
+		}
 		
 	}
 	
