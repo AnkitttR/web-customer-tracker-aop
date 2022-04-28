b792d8f2dd78ef724bf7c051f00e754d510cac95
Ankit <ankitthakur1998@gmail.com>
2022-04-28 14:37:04 +0530

    @Pointcut is used very first time in MyDemoLoggingAspect

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -12,10 +13,13 @@ public class MyDemoLoggingAspect {
 	
 	//let's start with an @Before advice
 	
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
 	//@Before("execution(public void add*())")  //any method call void addAccount() please call this
 	//@Before("execution(void add*())") //any method call void add*() please call this
 	//@Before("execution(* add*(..))") //any method call "any return type" add*() please call this
-	@Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")   //any method call "any return type" add*() please call this
+	@Before("forDaoPackage()")   //any method call "any return type" add*() please call this
 	public void beforeAddAccountAdvice() {
 		
 		System.out.println("\n=====>>> Executing @Before advice on method");
