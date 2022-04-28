583c13688c8e2ee31aac3d6944e02f206e734dd9
Ankit <ankitthakur1998@gmail.com>
2022-04-28 20:54:19 +0530

    @Pointcut added to LuvAopExpressions

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -9,24 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyDemoLoggingAspect {
 
-	// this is where we add all of our related advices for logging
 	
-	//let's start with an @Before advice
-	
-	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
-	private void forDaoPackage() {}
-	
-	// create pointcut for getter methods
-	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
-	private void getter() {}
-	
-	//create pointcut for setter methods
-	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
-	private void setter() {}
-	
-	//create pointcut: include package ...exclude getter/setter
-	@Pointcut("forDaoPackage() && !getter()||setter() ")
-	private void forDaoPackageNoGetterSetter() {}
 	
 	//@Before("execution(public void add*())")  //any method call void addAccount() please call this
 	//@Before("execution(void add*())") //any method call void add*() please call this
