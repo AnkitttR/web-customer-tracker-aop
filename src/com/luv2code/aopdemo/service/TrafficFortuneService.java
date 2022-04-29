05e1604cae8169c00b84f04532142bc3eb3a86bb
Ankit <ankitthakur1998@gmail.com>
2022-04-29 16:23:23 +0530

    AroundDemoApp class created

--- src/com/luv2code/aopdemo/service/TrafficFortuneService.java
+++ src/com/luv2code/aopdemo/service/TrafficFortuneService.java
@@ -7,12 +7,17 @@ public class TrafficFortuneService {
 	public String getFortune() {
 		
 		// simulate a delay 
-		TimeUnit.SECONDS.sleep(5);
+		try {
+			TimeUnit.SECONDS.sleep(5);
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}
 		
 		//return a fortune
+		return "Expect heavy traffic this morning";
 		
 		
-		return null;
 		
 		
 		
